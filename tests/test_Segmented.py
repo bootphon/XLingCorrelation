cb15de97c7831750d5566c3c2f7e605baf1cc757
baudetgl <baudetgl.depots.ensimag.fr>
2018-04-18 12:37:25 +0200

    test segmented

--- tests/test_Segmented.py
+++ tests/test_Segmented.py
@@ -1,13 +1,10 @@
-import sys
+
 import unittest
-# sys.path.insert(0, 'src/')
-import Segmented
+from xlingcorrelation import Segmented
 
 class test_Segmented(unittest.TestCase):
 
-    def test(self):
-        self.assertTrue(True)
-#
+
 #     @classmethod
 #     def setUpClass(cls): # once before everything #populate database for example
 #         pass
@@ -17,7 +14,7 @@ class test_Segmented(unittest.TestCase):
 #         pass
 #
     def setUp(self): # will run before each test
-        self.seg = Segmented.Segmented('tests/data/segmented.txt', 'tests/data/gold.txt', 'tests/data/ortholines.txt', 'unit', 'algo', 'corpus')
+        self.seg = Segmented('tests/data/segmented.txt', 'tests/data/gold.txt', 'tests/data/ortholines.txt', 'unit', 'algo', 'corpus')
 
         pass #could set up  new directroy in which to work+create objects needed
 
@@ -25,25 +22,20 @@ class test_Segmented(unittest.TestCase):
         pass #could erase that directory
 
     def test_freqtop(self):
-        # for element in self.seg.freq_top():
-        #     self.assert
-        self.assertCountEqual(self.seg.freq_top().most_common(), [('luhk', 2), ('siydhaxlayt', 1), ('layt', 1), ('dhaetsaxkaemerax', 1), ('yuwwaanaxsihtahp', 1), ('yuwwaanaxsiydhaxkaemerax', 1), ('yuwwaanaxsihtwihdhmaamiy', 1), ('dherzaxlihtaxlrehdlayt', 1), ('siy', 1), ('ehmeychehmyuwwaanaxsiy', 1)])
+        answer=[('luhk', 2), ('siydhaxlayt', 1), ('layt', 1), ('dhaetsaxkaemerax', 1), ('yuwwaanaxsihtahp', 1), ('yuwwaanaxsiydhaxkaemerax', 1), ('yuwwaanaxsihtwihdhmaamiy', 1), ('dherzaxlihtaxlrehdlayt', 1), ('siy', 1), ('ehmeychehmyuwwaanaxsiy', 1)]
+        self.assertCountEqual(self.seg.freq_top().most_common(), answer)
         # run python -m unittest test_Segmented.py
 
     def test_freqwords(self):
-        self.assertCountEqual(self.seg.freq_words().most_common(),[('look', 2), ('light', 1), ('see', 1)])
-# # TODO : assert
-#
-#
-# if __name__=='__main__':
-#     unittest.main() # use python test_Segmented.py
+        answer=[('look', 2), ('light', 1), ('see', 1)]
+        self.assertCountEqual(self.seg.freq_words().most_common(),answer)
+
+    def test_compute_words(self):
+        answer=['dherzaxlihtaxlrehdlayt', 'layt', 'dhaetsaxkaemerax', 'luhk', 'yuwwaanaxsihtahp', 'siydhaxlayt', 'ehmeychehmyuwwaanaxsiy', 'yuwwaanaxsihtwihdhmaamiy', 'yuwwaanaxsiydhaxkaemerax', 'siy']
+        self.assertCountEqual(self.seg.compute_words(),answer)
+
+
+if __name__=='__main__':
+    unittest.main() # use python test_Segmented.py
 
     # edge cases
-
-
-# seg = Segmented.Segmented('test/segmented.txt', 'test/gold.txt', 'test/ortholines.txt', 'unit', 'algo', 'corpus')
-# # print('freqtop ',seg.freq_top().most_common())
-# # print('freqtop2', seg.freq_top())
-# # print('freqwords ', seg.freq_words().most_common())
-# print('compute',list(seg.compute_words()))
-# print(seg._nb_words)
