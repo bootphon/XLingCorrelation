913dfa9506f15fdfc45add6441d18394da65e2da
baudetgl <baudetgl.depots.ensimag.fr>
2018-04-18 12:38:37 +0200

    test segmented

--- tests/test_Segmented.py
+++ tests/test_Segmented.py
@@ -4,7 +4,6 @@ from xlingcorrelation import Segmented
 
 class test_Segmented(unittest.TestCase):
 
-
 #     @classmethod
 #     def setUpClass(cls): # once before everything #populate database for example
 #         pass
@@ -15,7 +14,6 @@ class test_Segmented(unittest.TestCase):
 #
     def setUp(self): # will run before each test
         self.seg = Segmented('tests/data/segmented.txt', 'tests/data/gold.txt', 'tests/data/ortholines.txt', 'unit', 'algo', 'corpus')
-
         pass #could set up  new directroy in which to work+create objects needed
 
     def tearDown(self): # will run after each test
@@ -28,14 +26,13 @@ class test_Segmented(unittest.TestCase):
 
     def test_freqwords(self):
         answer=[('look', 2), ('light', 1), ('see', 1)]
-        self.assertCountEqual(self.seg.freq_words().most_common(),answer)
+        self.assertCountEqual(self.seg.freq_words().most_common(), answer)
 
     def test_compute_words(self):
         answer=['dherzaxlihtaxlrehdlayt', 'layt', 'dhaetsaxkaemerax', 'luhk', 'yuwwaanaxsihtahp', 'siydhaxlayt', 'ehmeychehmyuwwaanaxsiy', 'yuwwaanaxsihtwihdhmaamiy', 'yuwwaanaxsiydhaxkaemerax', 'siy']
-        self.assertCountEqual(self.seg.compute_words(),answer)
+        self.assertCountEqual(self.seg.compute_words(), answer)
 
+    # add edge cases
 
 if __name__=='__main__':
     unittest.main() # use python test_Segmented.py
-
-    # edge cases
