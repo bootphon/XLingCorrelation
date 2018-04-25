76ce6b8668d6ac7c87913f0666da6f0ebd17f88b
baudetgl <baudetgl.depots.ensimag.fr>
2018-04-25 11:39:59 +0200

    new tests + removed < > as in fr corpus

--- tests/data/pythontest.py
+++ tests/data/pythontest.py
@@ -3,12 +3,13 @@ import numpy as np
 import matplotlib.pyplot as plt
 from pandas import Series
 
-f= open("tags.txt",'r')
+# f= open("tags.txt",'r')
+f=open("../../../CDS/french/Lyon/tags-auto.txt", 'r')
 blop_init = f.read()
 f.close()
 
-print(blop_init)
-print('\n')
+# print(blop_init)
+# print('\n')
 
 """ WORDS """
 blop=blop_init.replace("\n", '')
@@ -17,7 +18,7 @@ blop=blop.replace(";esyll", '')
 blop=blop.replace(";eword", ' ')[:-1] # getting words alright
 
 print("Words")
-print(blop)
+# print(blop)
 print(len(blop.split()))
 
 
@@ -29,7 +30,7 @@ blop2 = blop2.replace(";eword", "")
 blop2 = blop2.replace(";esyll", " ")
 
 print("Syllables")
-print(blop2)
+# print(blop2)
 print(len(blop2.split()))
 
 
@@ -40,21 +41,24 @@ blop3 = blop3.replace(";eword", "")
 blop3 = blop3.replace(";esyll", "")
 
 print("Phones")
-print(blop3)
+# print(blop3)
 print(len(blop3.split()))
 
-c = Counter(blop3.split())
-print(c)
+c = Counter(blop.split())
+# print(c)
 # labels, values = zip(*Counter(blop3.split()).items())
-# indexes = np.arange(len(labels))
-# width = 1
-#
-# plt.bar(indexes, values, width)
-# plt.xticks(indexes + width * 0.5, labels)
+labels, values = zip(*c.most_common(100))
+indexes = np.arange(len(labels))
+width = 1
 
-# s=Series(blop3.split())
+plt.bar(indexes, values, width)
+plt.xticks(indexes + width * 0.5, labels)
+
+# s=Series(blop2.split())
 # vc = s.value_counts()
-# vc.plot(kind='bar')
+# vc.plot()
+
+
 
 plt.show()
 
@@ -67,7 +71,7 @@ for b in blop4:
         iso.append(b.replace(";eword","").replace(";esyll","").replace(" ",""))
 
 print("Isolated words")
-print(Counter(iso))
+# print(Counter(iso))
 
 
 """ Monosyllabic words """
@@ -79,4 +83,4 @@ for b in blop5 :
         mono.append(b.replace(";eword","").replace(";esyll","").replace(" ","").replace("\n",""))
 
 print("Monosyllabic words")
-print(Counter(mono))
+# print(Counter(mono))
