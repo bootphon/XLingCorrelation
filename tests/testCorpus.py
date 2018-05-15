7507307d05c0d1c6cd6bf6cfec0943e346172619
baudetgl <baudetgl.depots.ensimag.fr>
2018-05-15 12:26:30 +0200

    cleaned out tests

--- tests/testCorpus.py
+++ tests/testCorpus.py
@@ -6,7 +6,31 @@ pb imports
 pb path - when calling scripts -
 """
 
-corpus = Corpus.Corpus("~/Desktop/PFE/package/experiments/")
+# corpus = Corpus.Corpus("~/Desktop/PFE/package/experiments/")
+c0 = Corpus.Corpus("/Users/gladysbaudet/Desktop/PFE/CDS/english/Brent/full_corpus/tags.txt")
+c1 = Corpus.Corpus("/Users/gladysbaudet/Desktop/PFE/CDS/french/Lyon/tags-auto-updated-clean.txt")
+c2 = Corpus.Corpus("/Users/gladysbaudet/Desktop/PFE/CDS/spanish/OreaPine/tags-auto.txt")
+c3 = Corpus.Corpus("/Users/gladysbaudet/Desktop/PFE/CDS/danish/Plunkett/tags3.txt")
+c4 = Corpus.Corpus("/Users/gladysbaudet/Desktop/PFE/CDS/swedish/Lund/tags.txt")
+
+print("english")
+print(c0.get_nb_isolated_words()/c0.get_nb_words())
+print(c0.get_nb_monosyllabic_words()/c0.get_nb_words())
+print("french")
+print(c1.get_nb_isolated_words()/c1.get_nb_words())
+print(c1.get_nb_monosyllabic_words()/c1.get_nb_words())
+#
+print("spanish")
+print(c2.get_nb_isolated_words()/c2.get_nb_words())
+print(c2.get_nb_monosyllabic_words()/c2.get_nb_words())
+print("danish")
+print(c3.get_nb_isolated_words()/c3.get_nb_words())
+print(c3.get_nb_monosyllabic_words()/c3.get_nb_words())
+print("swedish")
+print(c4.get_nb_isolated_words()/c4.get_nb_words())
+print(c4.get_nb_monosyllabic_words()/c4.get_nb_words())
+
+
 
 # corpus.compute_all_cha()
 # corpus.clean_annotations(extra="~/Desktop/PFE/package/experiments/extraclean.sh")
--- tests/test_ListModels.py
+++ tests/test_ListModels.py
@@ -1,6 +1,8 @@
 from collections import Counter
-from xlingcorrelation import Segmented, Reports, ListModels, Model
+from xlingcorrelation import Segmented, Reports, ListModels, Model, Corpus
 # import numpy as np
+import matplotlib.pyplot as plt
+import statsmodels.api as sm
 
 path = '/Users/gladysbaudet//Desktop/PFE/'
 # cds = 'CDS/english/Brent/Brent/'
@@ -23,49 +25,17 @@ unit = 'syllable/'
 # np.random.seed(2)
 
 # listsegfull = [Segmented("tests/data/segmented.txt", "tests/data/gold.txt", "tests/data/ortholines.txt", "unit", "algo", "cds")]
-# listsegfull = [Segmented(path+'Results/'+size+algo+unit+'segmented.txt', path+cds+'gold.txt', path+cds+'ortholines.txt', unit, algo, cds) for algo in algos]
-#listsegfull = [Segmented(path+cds+'gold.txt', path+cds+'gold.txt', path+cds+'ortholines.txt', 'unit', 'gold', 'cds')]
-# listsegfull = [Segmented(path+cds+'gold.txt', path+cds+'gold.txt', path+cds+'ortholines-limited-updated.txt', 'unit', 'gold', 'cds')]
-
+listsegfull = [Segmented(path+'Results/'+size+algo+unit+'segmented.txt', path+cds+'gold.txt', path+cds+'ortholines.txt', unit, algo, cds) for algo in algos]
+''' TODO change architecture of results !!! '''
 
 print(cds)
-# print("daddy : ",listsegfull[0]._freq_words['daddy'])
-# print("daediy : ", listsegfull[0]._freq_top['daediy'])
-# print("mommy : ",listsegfull[0]._freq_words['mommy'])
-# print("maamiy : ", listsegfull[0]._freq_top['maamiy'])
 
 # rep = Reports("tests/data/reports.csv")
 rep = Reports(path+"CDI/english/Prop_WG_understands_8_18.csv")
+rep_ortho = rep.get_reports(13)
 
-# listortho=Counter()
-ortho=open(path+cds+"ortholines.txt",'r').read()
-rep_ortho=rep.get_reports(8)
-seg_ortho=Counter(ortho.lower().split())
-print(seg_ortho['mommy'])
-mod_ortho=Model(seg_ortho,rep_ortho)
-mod_ortho.compute()
+models = ListModels(listsegfull, rep)
 
-print("Nb words : ", len(mod_ortho._data))
-print(mod_ortho._lin_reg)
-mod_ortho.plot()
-# models = ListModels(listsegfull, rep)
-#
-# models.compute()
-# models.plot_regression(5,0)
-# print(models._df)
-# print(models.)
-
-
-# listsegfull = [Segmented.Segmented(path+'Results/'+size+algo+unit+'segmented.txt', path+cds+'gold.txt', path+cds+'ortholines.txt', unit, algo, cds) for algo in algos]
-# # seg1 = Segmented.Segmented(path+'Results/20k/dibs/syllable/segmented0.txt', path+'CDS/english/Brent/20k/gold0.txt', path+'CDS/english/Brent/sub_corpus/20k/ortholines0.txt')
-#
-# # listseg = [listsegfull[0]]
-#
-# rep = Reports.Reports(path+"CDI/english/Prop_WG_understands_8_18.csv")
-# models2 = ListModels.ListModels(listsegfull, rep)
-#
-# # print('intersect', models._results[0,0]._data)
-# models2.compute()
-# print(models2._df.head(5))
-# print(models._df[models._df['age']==13])
-# print(models2._df[models2._df['age']==13])
+models.compute()
+models.plot_regression(2,0)
+print(models._df.head(5))
